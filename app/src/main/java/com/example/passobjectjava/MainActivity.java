66927ae8cd6f5a49f096180e65d9916a4687b986
MirzalievPulat <mirzaliyevpolat3105@gmail.com>
2022-12-11 15:04:56 +0500

    returns "thank you"

--- app/src/main/java/com/example/passobjectjava/MainActivity.java
+++ app/src/main/java/com/example/passobjectjava/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.passobjectjava;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.passobjectjava.model.User;
 
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = MainActivity.class.toString();
+    TextView text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         initviews();
     }
     void initviews(){
+        text2 = findViewById(R.id.text);
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,9 +36,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    ActivityResultLauncher<Intent> secondLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == RESULT_OK){
+                        Intent data = result.getData();
+                        String text = data.getStringExtra("result");
+                        Log.d(TAG,text);
+                        text2.setText(text);
+                    }
+                }
+            }
+    );
+
     void openSecondPage(User user){
         Intent intent = new Intent(this,SecondActivity.class);
         intent.putExtra("user", user);
-        startActivity(intent);
+        secondLauncher.launch(intent);
     }
 }
--- app/src/main/java/com/example/passobjectjava/SecondActivity.java
+++ app/src/main/java/com/example/passobjectjava/SecondActivity.java
@@ -2,8 +2,12 @@ package com.example.passobjectjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.passobjectjava.model.User;
@@ -19,10 +23,24 @@ public class SecondActivity extends AppCompatActivity {
 
     void initviews(){
         TextView text = findViewById(R.id.text);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBack();
+            }
+        });
 
         User user = (User) getIntent().getParcelableExtra("user");
         Log.d(TAG,user.toString());
 
         text.setText(user.toString());
+    }
+
+    void openBack(){
+        Intent intent = new Intent();
+        intent.putExtra("result", "Thank You");
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
